ccda1a412634ab15d7ab4d47534e8d8a149f70d2
JohanScheepersf <scheepersjohanf@gmail.com>
2022-03-07 13:44:22 +0200

    First

--- payload-formatters/lmic-node-uplink-formatters.js
+++ payload-formatters/lmic-node-uplink-formatters.js
@@ -4,24 +4,28 @@
  * 
  *  Function:     LMIC-node uplink payload formatter JavaScript function(s).
  * 
- *  Author:       Leonel Lopes Parente
+ *  Author:       Johan  Scheepers
  * 
  *  Description:  These function(s) are for use with The Things Network V3. 
  *                 
  ******************************************************************************/
 
-function decodeUplink(input) {
+
+ function decodeUplink(input) {
     var data = {};
     var warnings = [];
 
-    if (input.fPort == 10) {
-        data.counter = (input.bytes[0] << 8) + input.bytes[1];
-    }
-    else {
-        warnings.push("Unsupported fPort");
-    }
-    return {
-        data: data,
-        warnings: warnings
-    };
+
+  if (input.fPort == 10) {
+      data.tempA = ((input.bytes[0] & 0x80 ? 0xFFFF<<16 : 0) | input.bytes[0]<<8 | input.bytes[1])/100;
+      data.door = input.bytes[2];
+      data.watchdog = input.bytes[3];
+  }
+  else {
+      warnings.push("Unsupported fPort");
+  }
+  return {
+    data: data,
+    warnings: warnings
+  };
 }
